9fd08034f6b3755b21e76ab315f5f780f3f5e264
Pablo Garcia <pgarciacamou@gmail.com>
2014-04-27 01:20:58 -0700

    Tests done correctly

--- src/customDirective/cdCode.js
+++ src/customDirective/cdCode.js
@@ -1,5 +1,6 @@
 angular.module('CustomDirectives', ["ngRoute"])
 .directive('cdCode', [function () {
+	var cdCodeRegExp = /(?:(?:data|x)[-:_])?cd[-:_]code\s*?=\s*?".*?"/i;
 	return {
 		priority: 9000
 		,restrict: 'A'
@@ -9,6 +10,8 @@ angular.module('CustomDirectives', ["ngRoute"])
 			var element = iElement[0], 
 			options = iAttrs.cdCode.split(':');
 
+			// console.log(options);
+
 			iAttrs.cdCode = options[0];
 			iAttrs.flag = options[1];
 
@@ -17,12 +20,12 @@ angular.module('CustomDirectives', ["ngRoute"])
 				inScope: $scope, // Current Scope
 				inActual: iAttrs.flag === "inActual" && (function($scope, prop){
 					do {
+						$scope = $scope.$parent;
 						if($scope.hasOwnProperty(prop)) return $scope;
-					} while($scope = $scope.$parent);
+					} while($scope.$parent);
 					return $scope; // this is the outter most parent just in case it did not find any.
 				})($scope, iAttrs.cdCode) // Existing Variable within the Scope or any Parent
 			}, selectedScope;
-			// console.log(scopes.inActual);
 
 			selectedScope = scopes[iAttrs.flag] || scopes.inScope;
 			selectedScope[iAttrs.cdCode] = element.outerHTML.replace(cdCodeRegExp,"");
--- test/cdCode.spec.js
+++ test/cdCode.spec.js
@@ -3,9 +3,79 @@ describe("Custom Directives", function() {
 		var $injector = angular.injector(['CustomDirectives']);
 		expect($injector).toBeDefined();
 	});
+
+	beforeEach(module("CustomDirectives"));
+
 	describe("cdCode Directive", function() {
-		it("should behave...", function() {
-			expect(true).toBeTruthy();
+		var $compile, $scope, $rootScope;
+
+		beforeEach(inject(function (_$compile_, _$rootScope_){
+			$compile = _$compile_;
+			$rootScope = _$rootScope_;
+			$scope = $rootScope.$new();
+		}));
+
+		it("should find the directive", inject(function(_cdCodeDirective_) {
+			expect(_cdCodeDirective_).toBeDefined();
+		}));
+
+		it("should compile", function() {
+			expect($compile('<div data-cd:code="testDiv:inScope"></div>')).not.toBeFalsy();
+			expect($compile('<div data-cd-code="testDiv:inParent"></div>')).not.toBeFalsy();
+			expect($compile('<div data:cd:code="testDiv:inActual"></div>')).not.toBeFalsy();
+
+			expect($compile('<div data-cd:code="testDiv:inScope"></div>')($rootScope.$new())[0].nodeName).toBe("DIV");
+			expect($compile('<div data-cd-code="testDiv:inParent"></div>')($rootScope.$new())[0].nodeName).toBe("DIV");
+			expect($compile('<div data:cd:code="testDiv:inActual"></div>')($rootScope.$new())[0].nodeName).toBe("DIV");
 		});
-	});
+
+		describe("Scopes hierarchy", function() {
+			var actualAncestor, parentScope, thisScope;
+
+			beforeEach(inject(function (){
+				actualAncestor = $scope.$new();
+				parentScope = actualAncestor.$new();
+				thisScope = parentScope.$new();
+				$rootScope.testDiv = actualAncestor.testDiv = null;
+			}));
+
+			it("should declare the variable in the CURRENT scope", function() {
+				$compile('<div data-cd:code="testDiv:inScope"></div>')(thisScope);
+
+				expect(thisScope.hasOwnProperty("testDiv")).toBeTruthy();
+				expect(parentScope.hasOwnProperty("testDiv")).toBeFalsy();
+				expect(actualAncestor.testDiv).toBeNull();
+				expect($rootScope.testDiv).toBeNull();
+			});
+
+			it("should declare the variable in the PARENT scope", function() {
+				$compile('<div data-cd:code="testDiv:inParent"></div>')(thisScope);
+
+				expect(thisScope.hasOwnProperty("testDiv")).toBeFalsy();
+				expect(parentScope.hasOwnProperty("testDiv")).toBeTruthy();
+				expect(actualAncestor.testDiv).toBeNull();
+				expect($rootScope.testDiv).toBeNull();
+			});
+
+			it("should declare the variable in the ANCESTOR scope", function() {
+				$compile('<div data-cd:code="testDiv:inActual"></div>')(thisScope);
+
+				expect(thisScope.hasOwnProperty("testDiv")).toBeFalsy();
+				expect(parentScope.hasOwnProperty("testDiv")).toBeFalsy();
+				expect(actualAncestor.testDiv).not.toBeNull();
+				expect($rootScope.testDiv).toBeNull();
+			});
+
+			it("should declare the variable in the OUTERMOST scope", function() {
+				delete actualAncestor.testDiv;
+				$compile('<div data-cd:code="testDiv:inActual"></div>')(thisScope);
+
+				expect(thisScope.hasOwnProperty("testDiv")).toBeFalsy();
+				expect(parentScope.hasOwnProperty("testDiv")).toBeFalsy();
+				expect(actualAncestor.hasOwnProperty("testDiv")).toBeFalsy();
+				expect($rootScope.testDiv).not.toBeNull();
+			});
+		});
+
+});
 });

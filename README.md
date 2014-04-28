1fe4218296dd02b12c7715870312883d996fc6fd
Pablo Garcia <pgarciacamou@gmail.com>
2014-04-27 17:00:21 -0700

    Fixed identation. Identing correctly

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ cdCode
 ======
 
 AngularJS custom directive to display code within the markup. Grabs the code of the element that holds the directive and makes it available in the scope.
+
+Using:
+1. HighlightJS :: http://highlightjs.org/
+2. Angular-HighlightJS :: https://github.com/pc035860/angular-highlightjs
--- src/customDirective/cdCode.js
+++ src/customDirective/cdCode.js
@@ -1,31 +1,48 @@
 angular.module('CustomDirectives', ["ngRoute"])
 .directive('cdCode', [function () {
-	var cdCodeRegExp = /(?:(?:data|x)[-:_])?cd[-:_]code\s*?=\s*?".*?"/i;
+	var cdCodeRegExp = /\s?(?:(?:data|x)[-:_])?cd[-:_]code\s*?=\s*?".*?"/i
+	,lastTabs = /(\t*)(.+?)$/;
+
 	return {
-		priority: 9000
+		priority: 0
 		,restrict: 'A'
 		,scope: false
-		,link: function ($scope, iElement, iAttrs) {
-			if(!iAttrs.cdCode) return;
-			var element = iElement[0], 
-			options = iAttrs.cdCode.split(':');
+		,compile: function compile(tElement, tAttrs) {
+			var init = tElement[0].outerHTML.replace(cdCodeRegExp,'')
+			,identedTabs = init.match(lastTabs) // We match for the tabs of last closing tag in the string
+			,identedTabsRegExp; // We need to remove as many tabs as needed to have the code display properly
 
-			iAttrs.cdCode = options[0];
-			iAttrs.flag = options[1];
+			// If the last closing tag had tabs at the beginning
+			// We count them and create a regular expresion to remove them in every line.
+			if(identedTabs && identedTabs.length>1) 
+				identedTabsRegExp = new RegExp("^\t{"+identedTabs[1].length+"}","mg");
 
-			var scopes = {
-				inParent: $scope.$parent, // Parent Scope
-				inScope: $scope, // Current Scope
-				inActual: iAttrs.flag === "inActual" && (function($scope, prop){
-					do {
-						if($scope.hasOwnProperty(prop) || !$scope.$parent) return $scope;
-						$scope = $scope.$parent;
-					} while($scope);
-				})($scope, iAttrs.cdCode) // Existing Variable within the Scope or any Parent
-			}, selectedScope;
+			// Remove tabs from initial code:
+			identedTabsRegExp && (init = init.replace(identedTabsRegExp, ''));
 
-			selectedScope = scopes[iAttrs.flag] || scopes.inScope;
-			selectedScope[iAttrs.cdCode] = element.outerHTML.replace(cdCodeRegExp,"");
+			return function link($scope, iElement, iAttrs) {
+				if(!iAttrs.cdCode) return;
+				var options = iAttrs.cdCode.split(':')
+				,element = identedTabsRegExp? iElement[0].outerHTML.replace(identedTabsRegExp,'') : iElement[0].outerHTML;
+
+				iAttrs.cdCode = options[0];
+				iAttrs.flag = options[1];
+
+				var scopes = {
+					inParent: $scope.$parent // Parent Scope
+					,inScope: $scope // Current Scope
+					,inActual: iAttrs.flag === "inActual" && (function($scope, prop){
+						do {
+							if($scope.hasOwnProperty(prop) || !$scope.$parent) return $scope;
+							$scope = $scope.$parent;
+						} while($scope);
+					})($scope, iAttrs.cdCode) // Existing Variable within the Scope or any Parent
+				}
+				,selectedScope;
+
+				selectedScope = scopes[iAttrs.flag] || scopes.inScope;
+				selectedScope[iAttrs.cdCode] = {code: element.replace(cdCodeRegExp,''), compiled: init};
+			}
 		}
 	};
-}])
+}]);
